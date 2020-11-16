bad0a5c846c671ff6706aaf2f2986e6e7a096713
Kevin Long <kevin.uu.long@gmail.com>
2020-11-16 12:15:28 -0500

    Support for more filetypes
    The functions have been collapsed into a singular function that can handle singular or multiple sources as well as many more output filetypes

--- index.js
+++ index.js
@@ -1,83 +1,34 @@
 const shell = require('node-powershell');
+const path = require('path');
 
-// Example Object Format
-// var obj = {
-//     "C:\\Users\\Kevin Long\\AppData\\Local\\slack\\slack.exe": "C:\\users\\Kevin Long\\Documents\\GitHub\\program-icon-extractor\\output\\slack.bmp",
-//     "C:\\Users\\Kevin Long\\AppData\\Roaming\\Spotify\\Spotify.exe": "C:\\users\\Kevin Long\\Documents\\GitHub\\program-icon-extractor\\output\\Spotify.bmp"
-// }
+// Example Array Format
+// var programs = [
+// 	"C:\\Users\\USERNAME\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe",
+// 	"C:\\Users\\USERNAME\\AppData\\Local\\slack\\slack.exe"
+// ]
 
 // Test Cases
-// toBmp("C:\Users\eld-longkw\AppData\Local\slack\\slack.exe", "C:\\users\\eld-longkw\\Documents\\slack.bmp");
-// toBmps(obj);
-// toPng("C:\\Users\\eld-longkw\\AppData\\Local\\slack\\slack.exe", "C:\\users\\eld-longkw\\Documents\\slack.png")
-
-// TODO:
-// Collapse this into two functions that take an optional filetype
-// May also want to make it so that only a directory is required for the output
+// extract("C:\\Users\\USERNAME\\AppData\\Local\\slack\\slack.exe", "C:\\Users\\USERNAME\\Documents", "png")
+// extract(programs, "C:\\Users\\USERNAME\\Documents\\GitHub\\program-icon-extractor\\output", "png")
 
 function registerShell() {
-    return new shell({
-        executionPolicy: 'Bypass',
-        noProfile: true
-    });
+	return new shell({
+		executionPolicy: 'Bypass',
+		noProfile: true
+	});
 }
 
-function toBmp(programPath, destinationPath) {
-    ps = registerShell();
-    ps.addCommand(`Add-Type -AssemblyName System.Drawing`)
-    ps.addCommand(`[System.Drawing.Icon]::ExtractAssociatedIcon('${programPath}').ToBitmap().Save('${destinationPath}')`)
-    ps.invoke().then(output => {
-        console.log(output);
-    }).catch(err => {
-        console.log(err);
-        ps.dispose();
-    });
-}
-
-function toBmps(obj) {
-    ps = registerShell();
-    ps.addCommand(`Add-Type -AssemblyName System.Drawing`)
-    var programPaths = Object.keys(obj),
-        destinationPaths = Object.values(obj);
-    if (programPaths.length == destinationPaths.length) {
-        for (i = 0; i < programPaths.length; i++) {
-            ps.addCommand(`[System.Drawing.Icon]::ExtractAssociatedIcon('${programPaths[i]}').ToBitmap().Save('${destinationPaths[i]}')`)
-        }
-    }
-    ps.invoke().then(output => {
-        console.log(output);
-    }).catch(err => {
-        console.log(err);
-        ps.dispose();
-    });
-}
-
-function toPng(programPath, destinationPath) {
-    ps = registerShell();
-    ps.addCommand(`Add-Type -AssemblyName System.Drawing`)
-    ps.addCommand(`[System.Drawing.Icon]::ExtractAssociatedIcon('${programPath}').ToBitmap().Save('${destinationPath}','PNG')`)
-    ps.invoke().then(output => {
-        console.log(output);
-    }).catch(err => {
-        console.log(err);
-        ps.dispose();
-    });
-}
-
-function toPngs(obj) {
-    ps = registerShell();
-    ps.addCommand(`Add-Type -AssemblyName System.Drawing`)
-    var programPaths = Object.keys(obj),
-        destinationPaths = Object.values(obj);
-    if (programPaths.length == destinationPaths.length) {
-        for (i = 0; i < programPaths.length; i++) {
-            ps.addCommand(`[System.Drawing.Icon]::ExtractAssociatedIcon('${programPaths[i]}').ToBitmap().Save('${destinationPaths[i]}','PNG')`)
-        }
-    }
-    ps.invoke().then(output => {
-        console.log(output);
-    }).catch(err => {
-        console.log(err);
-        ps.dispose();
-    });
+function extract(programPath, destinationPath, format = "jpeg") {
+	const programPaths = programPath => [].concat(programPath)
+	console.log(programPaths(programPath));
+	ps = registerShell();
+	ps.addCommand(`Add-Type -AssemblyName System.Drawing`)
+	programPaths(programPath).forEach(element => {
+		var finalDestinationPath = path.join(destinationPath, `${path.basename(element, path.extname(element))}.${format}`);
+		ps.addCommand(`[System.Drawing.Icon]::ExtractAssociatedIcon('${element}').ToBitmap().Save('${finalDestinationPath}','${format}')`)
+	});
+	ps.invoke().then().catch(err => {
+		console.log(err);
+		ps.dispose();
+	});
 }
